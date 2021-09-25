a2b9f7d92f431691f0a9e0044ea5744a366d7bb2
Ishitva Verma <i_shit@Ishitvas-MacBook-Air.local>
2021-09-25 18:19:33 +0530

    get method

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+app.get("/",function(req,res)
+{
+  //console.log(req);
+  //respone.send("hello");
+  res.sendFile(__dirname+"/index.html");
+});
+app.get("/contacts",function(req,respone)
+{
+  respone.send("contacts");
+});
+
 app.listen(3000,function(){
   console.log("The server is started at 3000");
 });
