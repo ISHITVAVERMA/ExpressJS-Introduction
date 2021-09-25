b0c94747a14adca773eb8aefbfcef1332cf09e5b
Ishitva Verma <i_shit@Ishitvas-MacBook-Air.local>
2021-09-25 18:41:13 +0530

    final commit

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res)
 {
   //console.log(req);
   //respone.send("hello");
   res.sendFile(__dirname+"/index.html");
 });
-app.get("/contacts",function(req,respone)
+
+app.post("/",function(req,res)
 {
-  respone.send("contacts");
+  console.log(req.body);
+  var num1=Number(req.body.num1);
+  var num2=Number(req.body.num2);
+  var sum=num1+num2;
+  res.send("Hello there the sum of the number is : " +sum);
 });
+
+// app.get("/contacts",function(req,respone)
+// {
+//   respone.send("contacts");
+// });
 
 app.listen(3000,function(){
   console.log("The server is started at 3000");
